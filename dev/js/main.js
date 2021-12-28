7efb81801b1f1ea4dc8dc68987edb3d41bcbb0cc
Olga1001 <zhuravel.olga95@gmail.com>
2021-12-28 18:12:11 +0200

    update

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,16 +1,36 @@
 let btnPlus = document.querySelectorAll('.btn-calc_plus'),
     btnMinus = document.querySelectorAll('.btn-calc_minus'),
-    inputQty = document.querySelectorAll('.calc-qty');
+    inputQty = document.querySelectorAll('.calc-qty'),
+    calc = document.querySelectorAll('.calc'),
+    tabs = document.querySelectorAll('.tabs-discription li'),
+    contents = document.querySelectorAll('.content-discription .content-item');
 
-function addQty(qty) {
-    qty.value += 1;
-    if (qty.value != 0 && qty.value != 1) {
+function changeQty(qty,action) {
+    if (action == 'plus') {
+        qty.value = parseInt(qty.value) + 1;
+    } else if (action == 'minus') {
+        qty.value = parseInt(qty.value) - 1;
+    }
+    if (qty.value > 1) {
         qty.previousElementSibling.disabled = false;
     } else {
         qty.previousElementSibling.disabled = true;
+        qty.value = 1;
     }
 }
-for (let i = 0; i < array.length; i++) {
-    btnPlus[i].addEventListener('click', addQty(inputQty[i]))
+
+//+/- btns quantity
+for (let i = 0; i < calc.length; i++) {
+    btnPlus[i].addEventListener('click', () => changeQty(inputQty[i],'plus'))
+    btnMinus[i].addEventListener('click', () => changeQty(inputQty[i],'minus'))
 }
 
+//descriptions
+for (let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener('click', () => {
+        tabs[i].closest('.tabs-discription').querySelector('.active').classList.remove('active');
+        contents[i].closest('.content-discription').querySelector('.active').classList.remove('active');
+        tabs[i].classList.add('active');
+        contents[i].classList.add('active');
+    })
+}
--- dist/js/main.js
+++ dist/js/main.js
@@ -2,18 +2,53 @@
 
 var btnPlus = document.querySelectorAll('.btn-calc_plus'),
     btnMinus = document.querySelectorAll('.btn-calc_minus'),
-    inputQty = document.querySelectorAll('.calc-qty');
+    inputQty = document.querySelectorAll('.calc-qty'),
+    calc = document.querySelectorAll('.calc'),
+    tabs = document.querySelectorAll('.tabs-discription li'),
+    contents = document.querySelectorAll('.content-discription .content-item');
 
-function addQty(qty) {
-  qty.value += 1;
+function changeQty(qty, action) {
+  if (action == 'plus') {
+    qty.value = parseInt(qty.value) + 1;
+  } else if (action == 'minus') {
+    qty.value = parseInt(qty.value) - 1;
+  }
 
-  if (qty.value != 0 && qty.value != 1) {
+  if (qty.value > 1) {
     qty.previousElementSibling.disabled = false;
   } else {
     qty.previousElementSibling.disabled = true;
+    qty.value = 1;
   }
-}
+} //+/- btns quantity
 
-for (var i = 0; i < array.length; i++) {
-  btnPlus[i].addEventListener('click', addQty(inputQty[i]));
+
+var _loop = function _loop(i) {
+  btnPlus[i].addEventListener('click', function () {
+    return changeQty(inputQty[i], 'plus');
+  });
+  btnMinus[i].addEventListener('click', function () {
+    return changeQty(inputQty[i], 'minus');
+  });
+};
+
+for (var i = 0; i < calc.length; i++) {
+  _loop(i);
+} //descriptions
+
+
+var _loop2 = function _loop2(_i) {
+  tabs[_i].addEventListener('click', function () {
+    tabs[_i].closest('.tabs-discription').querySelector('.active').classList.remove('active');
+
+    contents[_i].closest('.content-discription').querySelector('.active').classList.remove('active');
+
+    tabs[_i].classList.add('active');
+
+    contents[_i].classList.add('active');
+  });
+};
+
+for (var _i = 0; _i < tabs.length; _i++) {
+  _loop2(_i);
 }
