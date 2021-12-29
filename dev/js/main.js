445cbd11342ceb0aa1d79369d934ce0e37c49a04
Olga1001 <zhuravel.olga95@gmail.com>
2021-12-29 13:09:20 +0200

    update

--- dev/js/main.js
+++ dev/js/main.js
@@ -3,7 +3,9 @@ let btnPlus = document.querySelectorAll('.btn-calc_plus'),
     inputQty = document.querySelectorAll('.calc-qty'),
     calc = document.querySelectorAll('.calc'),
     tabs = document.querySelectorAll('.tabs-discription li'),
-    contents = document.querySelectorAll('.content-discription .content-item');
+    contents = document.querySelectorAll('.content-discription .content-item'),
+    dataButton = document.querySelectorAll('[data-button]'),
+    closeBtn = document.querySelectorAll('[data-close]');
 
 function changeQty(qty,action) {
     if (action == 'plus') {
@@ -34,3 +36,86 @@ for (let i = 0; i < tabs.length; i++) {
         contents[i].classList.add('active');
     })
 }
+
+function toggleActive(getData, action) {
+    if (document.querySelector(`[data-item=${getData}]`)) {
+        if (action == true) {
+            document.querySelector(`[data-item=${getData}]`).classList.add('active')
+        } else {
+            document.querySelector(`[data-item=${getData}]`).classList.remove('active')
+        }
+    }
+}
+
+for (let i = 0; i < dataButton.length; i++) {
+    dataButton[i].addEventListener('click', () => toggleActive(dataButton[i].getAttribute('data-button'),true))
+    closeBtn[i].addEventListener('click', () => toggleActive(closeBtn[i].getAttribute('data-close'),false))
+}
+
+//zoom pdp
+function imageZoom(imgID, resultID) {
+    var img, lens, result, cx, cy;
+    img = document.getElementById(imgID);
+    result = document.getElementById(resultID);
+    /*create lens:*/
+    lens = document.createElement("DIV");
+    lens.setAttribute("class", "img-zoom-lens");
+    /*insert lens:*/
+    img.parentElement.insertBefore(lens, img);
+    /*calculate the ratio between result DIV and lens:*/
+    cx = result.offsetWidth / lens.offsetWidth;
+    cy = result.offsetHeight / lens.offsetHeight;
+    /*set background properties for the result DIV:*/
+    result.style.backgroundImage = "url('" + img.src + "')";
+    result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
+    /*execute a function when someone moves the cursor over the image, or the lens:*/
+    lens.addEventListener("mousemove", moveLens);
+    img.addEventListener("mousemove", moveLens);
+    /*and also for touch screens:*/
+    lens.addEventListener("touchmove", moveLens);
+    img.addEventListener("touchmove", moveLens);
+    function moveLens(e) {
+        var pos, x, y;
+        /*prevent any other actions that may occur when moving over the image:*/
+        e.preventDefault();
+        /*get the cursor's x and y positions:*/
+        pos = getCursorPos(e);
+        /*calculate the position of the lens:*/
+        x = pos.x - (lens.offsetWidth / 2);
+        y = pos.y - (lens.offsetHeight / 2);
+        /*prevent the lens from being positioned outside the image:*/
+        if (x > img.width - lens.offsetWidth) {x = img.width - lens.offsetWidth;}
+        if (x < 0) {x = 0;}
+        if (y > img.height - lens.offsetHeight) {y = img.height - lens.offsetHeight;}
+        if (y < 0) {y = 0;}
+        /*set the position of the lens:*/
+        lens.style.left = x + "px";
+        lens.style.top = y + "px";
+        /*display what the lens "sees":*/
+        result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
+    }
+    function getCursorPos(e) {
+        var a, x = 0, y = 0;
+        e = e || window.event;
+        /*get the x and y positions of the image:*/
+        a = img.getBoundingClientRect();
+        /*calculate the cursor's x and y coordinates, relative to the image:*/
+        x = e.pageX - a.left;
+        y = e.pageY - a.top;
+        /*consider any page scrolling:*/
+        x = x - window.pageXOffset;
+        y = y - window.pageYOffset;
+        return {x : x, y : y};
+    }
+}
+
+imageZoom("slider-for_img", "myresult");
+
+document.querySelector('.slider-for').addEventListener('mousemove', (e) => {
+    document.querySelector('.img-zoom-result').style.visibility = 'visible';
+    document.querySelector('.img-zoom-lens').style.visibility = 'visible';
+})
+document.querySelector('.slider-for').addEventListener('mouseout', (e) => {
+    document.querySelector('.img-zoom-result').style.visibility = 'hidden';
+    document.querySelector('.img-zoom-lens').style.visibility = 'hidden';
+})
--- dist/js/main.js
+++ dist/js/main.js
@@ -5,7 +5,9 @@ var btnPlus = document.querySelectorAll('.btn-calc_plus'),
     inputQty = document.querySelectorAll('.calc-qty'),
     calc = document.querySelectorAll('.calc'),
     tabs = document.querySelectorAll('.tabs-discription li'),
-    contents = document.querySelectorAll('.content-discription .content-item');
+    contents = document.querySelectorAll('.content-discription .content-item'),
+    dataButton = document.querySelectorAll('[data-button]'),
+    closeBtn = document.querySelectorAll('[data-close]');
 
 function changeQty(qty, action) {
   if (action == 'plus') {
@@ -52,3 +54,128 @@ var _loop2 = function _loop2(_i) {
 for (var _i = 0; _i < tabs.length; _i++) {
   _loop2(_i);
 }
+
+function toggleActive(getData, action) {
+  if (document.querySelector("[data-item=".concat(getData, "]"))) {
+    if (action == true) {
+      document.querySelector("[data-item=".concat(getData, "]")).classList.add('active');
+    } else {
+      document.querySelector("[data-item=".concat(getData, "]")).classList.remove('active');
+    }
+  }
+}
+
+var _loop3 = function _loop3(_i2) {
+  dataButton[_i2].addEventListener('click', function () {
+    return toggleActive(dataButton[_i2].getAttribute('data-button'), true);
+  });
+
+  closeBtn[_i2].addEventListener('click', function () {
+    return toggleActive(closeBtn[_i2].getAttribute('data-close'), false);
+  });
+};
+
+for (var _i2 = 0; _i2 < dataButton.length; _i2++) {
+  _loop3(_i2);
+} //zoom pdp
+
+
+function imageZoom(imgID, resultID) {
+  var img, lens, result, cx, cy;
+  img = document.getElementById(imgID);
+  result = document.getElementById(resultID);
+  /*create lens:*/
+
+  lens = document.createElement("DIV");
+  lens.setAttribute("class", "img-zoom-lens");
+  /*insert lens:*/
+
+  img.parentElement.insertBefore(lens, img);
+  /*calculate the ratio between result DIV and lens:*/
+
+  cx = result.offsetWidth / lens.offsetWidth;
+  cy = result.offsetHeight / lens.offsetHeight;
+  /*set background properties for the result DIV:*/
+
+  result.style.backgroundImage = "url('" + img.src + "')";
+  result.style.backgroundSize = img.width * cx + "px " + img.height * cy + "px";
+  /*execute a function when someone moves the cursor over the image, or the lens:*/
+
+  lens.addEventListener("mousemove", moveLens);
+  img.addEventListener("mousemove", moveLens);
+  /*and also for touch screens:*/
+
+  lens.addEventListener("touchmove", moveLens);
+  img.addEventListener("touchmove", moveLens);
+
+  function moveLens(e) {
+    var pos, x, y;
+    /*prevent any other actions that may occur when moving over the image:*/
+
+    e.preventDefault();
+    /*get the cursor's x and y positions:*/
+
+    pos = getCursorPos(e);
+    /*calculate the position of the lens:*/
+
+    x = pos.x - lens.offsetWidth / 2;
+    y = pos.y - lens.offsetHeight / 2;
+    /*prevent the lens from being positioned outside the image:*/
+
+    if (x > img.width - lens.offsetWidth) {
+      x = img.width - lens.offsetWidth;
+    }
+
+    if (x < 0) {
+      x = 0;
+    }
+
+    if (y > img.height - lens.offsetHeight) {
+      y = img.height - lens.offsetHeight;
+    }
+
+    if (y < 0) {
+      y = 0;
+    }
+    /*set the position of the lens:*/
+
+
+    lens.style.left = x + "px";
+    lens.style.top = y + "px";
+    /*display what the lens "sees":*/
+
+    result.style.backgroundPosition = "-" + x * cx + "px -" + y * cy + "px";
+  }
+
+  function getCursorPos(e) {
+    var a,
+        x = 0,
+        y = 0;
+    e = e || window.event;
+    /*get the x and y positions of the image:*/
+
+    a = img.getBoundingClientRect();
+    /*calculate the cursor's x and y coordinates, relative to the image:*/
+
+    x = e.pageX - a.left;
+    y = e.pageY - a.top;
+    /*consider any page scrolling:*/
+
+    x = x - window.pageXOffset;
+    y = y - window.pageYOffset;
+    return {
+      x: x,
+      y: y
+    };
+  }
+}
+
+imageZoom("slider-for_img", "myresult");
+document.querySelector('.slider-for').addEventListener('mousemove', function (e) {
+  document.querySelector('.img-zoom-result').style.visibility = 'visible';
+  document.querySelector('.img-zoom-lens').style.visibility = 'visible';
+});
+document.querySelector('.slider-for').addEventListener('mouseout', function (e) {
+  document.querySelector('.img-zoom-result').style.visibility = 'hidden';
+  document.querySelector('.img-zoom-lens').style.visibility = 'hidden';
+});
