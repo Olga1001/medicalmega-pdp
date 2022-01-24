adbbe040310158e54cfc31204d267ec36a88935e
Olga1001 <zhuravel.olga95@gmail.com>
2022-01-24 11:36:48 +0200

    update

---
+++ dist/js/slider.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var sliderCategories = tns({
+  container: document.querySelector('.product_sidebar .available-options .justify-content-between'),
+  items: 2,
+  axis: 'horizontal',
+  controls: true,
+  loop: false,
+  prevButton: document.querySelector('.arrow_button_prev'),
+  nextButton: document.querySelector('.arrow_button_next'),
+  autoplayButton: false,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  nav: false,
+  // autoWidth: true,
+  preventScrollOnTouch: 'auto',
+  swipeAngle: false
+});
